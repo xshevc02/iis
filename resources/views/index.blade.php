0ea86ce67839e9cd825d89f3d659340c70060b3d
xnovik03 <nika21731118825@gmail.com>
2024-11-20 22:29:05 +0100

    css setting

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Watch</title>
+   @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+</body>
+</html>
